c3788660eb577cb620c14355fef23285d6be2f64
boydgibson44 <boyd@pinnaclevl.com>
2024-10-02 16:09:27 +0100

    dist folder
    dist test

---
+++ BlazorApp/dist/typescripttesting.spec.ts
@@ -0,0 +1,13 @@
+﻿import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('http://localhost:5128/');
+    await page.getByRole('link', { name: 'Counter' }).click();
+    await page.getByRole('button', { name: 'Click me' }).click();
+    await page.getByRole('button', { name: 'Click me' }).click();
+    await page.getByRole('heading', { name: 'Counter' }).click();
+    await page.getByText('Current count:').click();
+    await page.getByRole('link', { name: 'Weather' }).click();
+    await page.getByRole('heading', { name: 'Weather' }).click();
+    await page.getByText('This component demonstrates').click();
+});
