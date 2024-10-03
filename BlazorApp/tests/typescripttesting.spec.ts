6cce6b0eec32ca2e154f752fb7a2fd576739ace0
boydgibson44 <boyd@pinnaclevl.com>
2024-10-03 11:05:44 +0100

    revert change

--- BlazorApp/tests/typescripttesting.spec.ts
+++ BlazorApp/tests/typescripttesting.spec.ts
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
     await page.goto('http://localhost:5128/');
     await page.getByRole('link', { name: 'Counter' }).click();
     await page.getByRole('button', { name: 'Click me' }).click();
-    await page.getByRole('button', { name: 'Click Me' }).click();
+    await page.getByRole('button', { name: 'Click me' }).click();
     await page.getByRole('heading', { name: 'Counter' }).click();
     await page.getByText('Current count:').click();
     await page.getByRole('link', { name: 'Weather' }).click();
